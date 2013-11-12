dac33734cd2c71542dc5ddf1d9c480936c4ca56e
Obed Eugene <obed.eugene@gmail.com>
2013-11-12 09:04:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShallengeApp::Application.config.secret_key_base = '9454d2cb9f08a627aaec977aa880e5a5ddcc33a8d3e06a7e8f07b37696e2215eb99cd9c927f2580b954dc86f21780d251b1883f31d56a6ea00eb9612278af269'
